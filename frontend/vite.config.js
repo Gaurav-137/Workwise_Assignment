e8b282b6d151003199e8754eca166d377f6d693f
Gaurav-137 <Gaurav-137@users.noreply.github.com>
2025-04-12 17:20:08 +0530

    Initial commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,6 @@
 // vite.config.js
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,          // Optional: Change to your desired dev port
-    open: true,          // Optional: Opens browser on dev start
-  },
-  resolve: {
-    alias: {
-      '@': '/src',       // Optional: Allows use of "@/components/..." in imports
-    },
-  },
-})
+export default {
+  build: {
+    outDir: 'build',  // Set this to 'build' (default) or 'dist' based on your preference.
+  }
+}
